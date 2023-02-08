9581868edecbe5a83ef6edcb00e1cfb73f126acf
GageMarkgraf <gmarkgra@uncc.edu>
2023-02-07 20:22:07 -0500

    I have no clue what is going on

--- colsort.c
+++ colsort.c
@@ -7,40 +7,7 @@ int drive_sort(int argc, char* argv[])
 {
     return 0;
 }
-char* file = argv;
-char tempString[128];
-file* ptrFile = NULL;
-char** data = NULL;
-int lines = 0;
 
-while(fgets(file, 128, ptrfile) != NULL){
-    tempString = (char**)malloc(data, sizeof(char**)*(lines+1));
-    data[lines] = (char*)malloc(128, sizeof(char));
-    strcpy(data[lines], tempString);
-}
-for(i= 0; i < (noOfLines - 1); ++i) {
-        for(j = 0; j < ( noOfLines - i - 1); ++j) {
-            if(strcmp(strData[j], strData[j+1]) > 0) {
-                strcpy(strTempData, strData[j]);
-                strcpy(strData[j], strData[j+1]);
-                strcpy(strData[j+1], strTempData);
-            }
-        }
-    }
-    for(i = 0; i < lines; i++)
-        fprintf(ptrSummary,"%s\n",strData[i]);
-    for(i = 0; i < noOfLines; i++)
-        free(strData[i]);
-    free(data);
-    fclose(ptrFile);
-    return 0;
-fopen(argv);
-fgets(argv);
-//strcmp();
-//qsort();
-//strtok();
-//fclose();
-//fprintf(stdout, argv);
 // TODO all changes should be made above this line
 int main(int argc, char* argv[])
 {
