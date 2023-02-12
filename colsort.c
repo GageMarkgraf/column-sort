7f9402b34a06473f5736dd7da70469ec6c4663bd
GageMarkgraf <gmarkgra@uncc.edu>
2023-02-11 20:05:59 -0500

    fgn

--- colsort.c
+++ colsort.c
@@ -14,9 +14,9 @@ int drive_sort(int argc, char* argv[])
     FILE* outFile;
     int i = 0;
     char a;
-    char str[] = (char)myarg[0];
+    //char str[] = (char)myarg[0];
     char del[] = " ";
-    char* ptr = strtok(str, del);
+    //char* ptr = strtok(str, del);
     int len = strlen(argv[1]);
     char* temp;
     char mat[10];
