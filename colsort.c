927a251165b2e995f2944605accabd3b264d5c87
GageMarkgraf <gmarkgra@uncc.edu>
2023-02-11 21:25:30 -0500

    tghjsdfg

--- colsort.c
+++ colsort.c
@@ -28,11 +28,11 @@ int drive_sort(int argc, char* argv[])
         return (0);
     }
 
-    fscanf(inFile, "%[^\n]", &str1, &str2, &str3);
+    fscanf(inFile, "%[^\n] %[^\n] %[^\n]", &str1, &str2, &str3);
 
-    fprintf(outFile, "%s", str1);
-    fprintf(outFile, "%s", str2);
-    fprintf(outFile, "%s", str3);
+    fprintf(outFile, "%s", &str1);
+    fprintf(outFile, "%s", &str2);
+    fprintf(outFile, "%s", &str3);
 
     do
     {
