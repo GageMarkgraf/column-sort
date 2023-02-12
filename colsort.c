79539f6d618039cf214f6c610b7830e12a15471b
GageMarkgraf <gmarkgra@uncc.edu>
2023-02-11 21:29:04 -0500

    fhj

--- colsort.c
+++ colsort.c
@@ -28,9 +28,10 @@ int drive_sort(int argc, char* argv[])
         return (0);
     }
 
-    fscanf(inFile, "%[^\n]", &str1);
-    fscanf(inFile, "%[^\n] %[^\n]", &str2);
-
+    for(int i = 0; i < MAX_LINES; i++)
+    {
+    fscanf(inFile, "%[^\n] %[^\n] %[^\n]\n", &str1, &str2, &str3);
+    }
     fprintf(outFile, "%s", &str1);
     fprintf(outFile, "%s", &str2);
     //fprintf(outFile, "%s", &str3);
