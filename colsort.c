b5d70cbb0177b2b14c3682b62f53256fdc8b6e6c
GageMarkgraf <gmarkgra@uncc.edu>
2023-02-11 21:59:12 -0500

    dfg

--- colsort.c
+++ colsort.c
@@ -14,7 +14,7 @@ int drive_sort(int argc, char* argv[])
     char a;
     char line[MAX];
     char tmp[MAX_LINES];
-    char str1[MAX];
+    char str1[500];
 
     if ((inFile = (fopen(argv[1], "r"))) == NULL)
     {
