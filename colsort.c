64434a6bece0f9d2525776e099d3117dc1f59cc4
GageMarkgraf <gmarkgra@uncc.edu>
2023-02-11 22:33:36 -0500

    dfg

--- colsort.c
+++ colsort.c
@@ -16,7 +16,7 @@ int drive_sort(int argc, char* argv[])
     char tmp[MAX_LINES];
     char c;
     char str1, str2, str3;
-    char* append = "%c[^\n]";
+    char append = "%c[^\n]";
 
     if ((inFile = (fopen(argv[1], "r"))) == NULL)
     {
@@ -34,7 +34,7 @@ int drive_sort(int argc, char* argv[])
     {
         fprintf(outFile, "%c", c);
     }   
-    c = append;
+    c = &append;
     scanf(c, "%[^\n] %[^\n] %[^\n]", str1, str2, str3);
 
     fprintf(outFile, "%c", str1);
